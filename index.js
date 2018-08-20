74f3a3afee1d4d289901d4ad265fdb4ef11665d7
ayesha ansari <ayesha.x.ansari@gmail.com>
2018-08-20 12:58:04 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -9,26 +9,4 @@ class BoardMember {
     return 'No, I must disagree'
   }
 
-  approve (){
-   return 'You can do that!'
-  }
-
-  doCharity () {
-    return "I like to help people."
-  }
-
-  releasePressStatement (){
-    return "You will see great things from Scuber."
-  }
-  sayHi (){
-    return `Hi, my name is ${this.name}. I am from ${this.homeState}, and I was trained in ${this.training}.`
-  }
-
-}
-
-class Ceo extends User {
-  hireEmployee () {
-    returns "Welcome aboard!"
-  }
-
 }
